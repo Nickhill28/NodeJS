5ee8e336879c25bdfdd34d4394c8986a030b8c46
nickhill28 <snickpick7@gmail.com>
2024-03-20 04:34:06 +0530

    seventh  node

--- nodeServer.js
+++ nodeServer.js
@@ -4,6 +4,9 @@ const server = http.createServer((req,res)=>{
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hi Im Nick\n');
+    console.log(req.method);
+  console.log(req.url);
+  console.log(req.headers);
     console.log('Nick');
 })
 server.listen(4000);
